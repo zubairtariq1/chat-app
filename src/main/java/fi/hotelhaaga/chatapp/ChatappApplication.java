88a5262ab0e0b4fdd093043a2d396b8c7479b3c7
Zubair Tariq <zubair_success747@yahoo.com>
2019-05-02 16:08:07 +0300

    Chat app built

---
+++ src/main/java/fi/hotelhaaga/chatapp/ChatappApplication.java
@@ -0,0 +1,13 @@
+package fi.hotelhaaga.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappApplication.class, args);
+	}
+
+}
